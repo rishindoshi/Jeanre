cd891865a15dcbdd79ad2350b90bf1630e4f4440
Rishin Doshi <rishdosh@umich.edu>
2016-08-12 21:46:07 -0400

    added some comments

--- server/app/routes.js
+++ server/app/routes.js
@@ -25,6 +25,12 @@ module.exports = function(app, api) {
 
 		classifier.getFeatures(genres, api)
 			.then(function(featureArray){
+				// We now have an array of feature objects
+				// Iterate through the array and filter each object to only include relevent features
+				// e.g Exclude keys such as (uri, duration, etc.)
+				// Then write the features to a file using node streams
+				// Then spawn child python process to classify input song
+				// Calculate percent accuracy and render results to front-end
 				console.log(featureArray);
 			})
 			.catch(function(err){
